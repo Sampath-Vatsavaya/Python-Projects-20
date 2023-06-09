305a57052131689db5825b2f922846fcb985c0c2
Sampath-Vatsavaya <vatsavaya.sampath@gmail.com>
2023-06-10 01:59:33 +0530

    mad libs project

---
+++ mad_libs.py
@@ -0,0 +1,130 @@
+def get_input(word_type: str):
+
+
+    user_input: str = input(f"Enter a {word_type}:")
+    return user_input
+
+noun1=get_input("noun")
+verb1=get_input("verb")
+verb2=get_input("verb")
+adjective1=get_input("adjective")
+adjective2=get_input("adjective")
+
+noun2=get_input("noun")
+verb3=get_input("verb")
+verb4=get_input("verb")
+adjective3=get_input("adjective")
+adjective4=get_input("adjective")
+
+noun3=get_input("noun")
+verb5=get_input("verb")
+verb6=get_input("verb")
+adjective5=get_input("adjective")
+adjective6=get_input("adjective")
+
+
+noun4=get_input("noun")
+verb7=get_input("verb")
+verb8=get_input("verb")
+adjective7=get_input("adjective")
+adjective8=get_input("adjective")
+
+story1=f"""
+Once upon a time, in a land far away, there were four colorful {noun1} who lived together in a cozy {noun2}. Each of them had their own unique personality and brought something special to the group.
+
+The first {noun1} was named {noun3}. They were known for their {adjective1} and {adjective2} nature. {noun3} loved to {verb1} through the meadows and {verb2} with the butterflies.
+
+The second {noun1} was called {noun4}. They were a bit shy but had a heart full of {adjective3}. {noun4} enjoyed {verb3} in the moonlight and {verb4} with the stars.
+
+The third member of the group was an adventurous {noun1} named {noun3}. With their {adjective4} spirit, they would often {verb5} into unknown territories and {verb6} hidden treasures.
+
+Lastly, there was the mischievous {noun1} known as {noun2}. They had a playful and {adjective5} personality. {noun4} would {verb7} around and {verb8} innocent pranks on their friends.
+
+Together, these four {noun1} embarked on many exciting adventures. They would {verb1} to enchanted forests, {verb2} across mystical rivers, and {verb3} up towering mountains. Their journeys were always filled with laughter, {adjective6} moments, and valuable life lessons.
+
+During their travels, they encountered various challenges. Sometimes they had to {verb4} their fears, other times they needed to {verb5} together to solve puzzles and overcome obstacles. Through it all, their strong bond and {adjective7} friendship helped them conquer any difficulty that came their way.
+
+Along their adventures, the {noun1} discovered hidden treasures and encountered magical creatures. They were amazed by the {adjective8} landscapes they encountered and the incredible beauty of the world around them.
+
+In the end, their shared experiences and memories created an unbreakable bond between the four {noun1}. They realized that no matter where they went or what they did, their friendship was the greatest treasure of all.
+
+And so, their story continues, filled with more laughter, new discoveries, and endless possibilities. These four {noun1} would forever cherish their time together and look forward to the next chapter of their incredible journey.
+"""
+
+story2=f"""Once upon a time, in a land of whimsy and wonder, there were four extraordinary {noun1} who embarked on a grand adventure. Each of them possessed a unique set of {adjective1} skills and a heart filled with {adjective2} dreams.
+
+The first member of the group was a courageous {noun2} named {noun3}. With their {adjective3} spirit and mighty {noun4}, they would fearlessly {verb1} into the unknown and {verb2} with boundless energy.
+
+The second companion was a clever {noun2} called {noun1}. Their sharp wit and {adjective4} mind allowed them to {verb3} intricate puzzles and {verb4} their way through any challenge they encountered.
+
+The third adventurer was a kind-hearted {noun2} named {noun3}. Their gentle nature and {adjective5} soul made them a beloved friend to all. They would often {verb5} in harmony with nature and {verb6} joy to those around them.
+
+Lastly, there was a mischievous {noun2} known as {noun4}. With their quick feet and a bag full of {adjective6} tricks, they would {verb7} through the shadows and {verb8} with laughter.
+
+Together, these four {noun1} set off on an epic quest. They would {verb1} through enchanted forests, {verb2} across treacherous seas, and {verb3} to the highest peaks of mountains. Their journey was filled with {adjective7} encounters and breathtaking landscapes.
+
+Throughout their adventures, they faced many trials and tribulations. They had to {verb4} dangerous creatures, navigate through dense {adjective8} mazes, and even {verb5} with powerful sorcery. But their unwavering friendship and indomitable spirit helped them overcome every obstacle.
+
+As they traveled, the four {noun1} discovered hidden realms and encountered legendary beings. They were awestruck by the {adjective7} wonders they witnessed and the endless possibilities that lay before them.
+
+In the end, their grand adventure not only brought them closer but also changed the fate of the entire land. Their bravery and {adjective8} actions inspired others to believe in the power of dreams and the strength of unity.
+
+And so, their tale continues, written in the annals of legends. The four brave {noun1} are forever bound by their shared memories and the incredible bond they forged along their journey. As they set off towards new horizons, they are ready to create even more extraordinary stories together."""
+
+story3=f""" Once upon a time, in a world filled with magic and mystery, there were four extraordinary {noun1} who possessed incredible powers. Each of them had their own distinct {adjective1} abilities and embarked on a quest to save their enchanted realm.
+
+The first member of the group was a wise {noun2} named {noun3}. With their ancient knowledge and {adjective2} intuition, they could {verb1} the secrets of the universe and {verb2} the elements at their command.
+
+The second companion was a fearless {noun2} known as {noun4}. Their unmatched courage and {adjective3} strength allowed them to {verb3} into battle and {verb4} the forces of darkness with their mighty sword.
+
+The third hero was a quick-witted {noun2} named {noun1}. Their sharp mind and {adjective4} intellect helped them unravel intricate puzzles and {verb5} through even the most cunning traps.
+
+Lastly, there was a mischievous {noun2} named {noun3}. With their clever tricks and {adjective5} charm, they could {verb6} their way out of any predicament and {verb7} a smile to the faces of their allies.
+
+Together, these four {noun1} set out on an epic journey. They would {verb1} through ancient ruins, {verb2} across vast oceans, and {verb3} to the highest peaks of towering mountains. Their path was fraught with danger, but their determination knew no bounds.
+
+Throughout their quest, they encountered formidable adversaries and tested their mettle against dark forces. They had to {verb4} against fearsome beasts, {verb5} through treacherous mazes, and even {verb6} the very fabric of reality. Yet, their unwavering resolve and the strength of their unity pushed them forward.
+
+As they traveled, the four {noun1} discovered hidden relics and encountered mythical creatures. They witnessed the awe-inspiring beauty of {adjective6} landscapes and the wonders of long-lost civilizations.
+
+In the end, their heroic deeds and selfless sacrifices saved the realm from certain doom. Their {adjective7} bravery inspired hope in the hearts of the people, and the land was forever grateful for their noble actions.
+
+And so, their legend echoes through the ages, a testament to the power of unity and the triumph of good over evil. The four valiant {noun1} continue to protect their world, their bond unbreakable and their spirits forever intertwined."""
+
+
+story4=f"""Once upon a time, in a whimsical land filled with wonder, there were four adventurous {noun1} who set out on a thrilling quest. Each of them possessed unique {adjective1} qualities and a burning desire for excitement.
+
+The first member of the group was a brave {noun2} named {noun3}. With their {adjective2} heart and swift {noun4}, they would fearlessly {verb1} into the unknown and {verb2} through the wildest of landscapes.
+
+The second companion was a cunning {noun2} called {noun1}. Their sharp intellect and {adjective3} wit allowed them to {verb3} their way out of tricky situations and {verb4} through ancient puzzles with ease.
+
+The third adventurer was a free-spirited {noun2} named {noun3}. Their vibrant energy and {adjective4} soul made them the life of every party. They would often {verb5} under the starry sky and {verb6} with joy wherever they went.
+
+Lastly, there was a mischievous {noun2} known as {noun4}. With their playful pranks and {adjective5} antics, they would {verb7} around and {verb8} laughter to their companions, turning every moment into a memorable one.
+
+Together, these four {noun1} embarked on a grand odyssey. They would {verb1} through enchanted forests, {verb2} across raging rivers, and {verb3} to the highest peaks of towering mountains. Their journey was filled with breathtaking vistas and unexpected encounters.
+
+Throughout their quest, they faced numerous challenges. They had to {verb4} mythical beasts, outwit cunning adversaries, and even {verb5} against the forces of darkness. Yet, their unyielding determination and boundless spirit carried them through every trial.
+
+As they traversed the lands, the four {noun1} discovered hidden treasures and encountered magical beings. They were mesmerized by the {adjective6} wonders they witnessed and the enchantment that surrounded them at every turn.
+
+In the end, their courageous exploits and unwavering friendship changed the fate of the land. They became legends, celebrated for their extraordinary deeds and {adjective7} hearts. Their tale would be told for generations, inspiring others to embrace their own adventures and cherish the bonds of camaraderie.
+
+And so, their story continues, filled with endless possibilities and new horizons to explore. The four intrepid {noun1} are forever united by the memories of their extraordinary journey, and their spirits remain forever etched in the tapestry of legends."""
+
+choice = int(input("Enter a Choice(1 to 4):"))
+match choice:
+    case 1:
+        print(story1)
+
+    case 2:
+        print(story2)
+
+    case 3:
+        print(story3)
+
+    case 4:
+        print(story4)
+
+    case _:
+        print("Invalid Choice")
